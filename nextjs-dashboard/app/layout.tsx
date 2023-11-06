fa094f56b10f585aeb3e64a96eccbb0c2ea5c85d
unknown <nicolasmuoz@gmail.com>
2023-11-06 16:45:10 -0500

    update

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,16 @@
+import { montserrat } from './ui/fonts';
+import './ui/global.css'
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className='py-10 flex justify-center items-center'>Hecho con &#128151; por Nicolas Muñoz</footer>
+      </body>
+    </html>
+  );
+}
